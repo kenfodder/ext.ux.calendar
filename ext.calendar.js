420397f6f5dda9e162600e4caefdf5bbd79eca9b
Kenneth Lee <kenfodder@gmail.com>
2011-02-06 01:19:08 +0000

    days show up correctly and selected day is highlighted

--- ext.calendar.js
+++ ext.calendar.js
@@ -16,8 +16,6 @@ Ext.ux.Calendar = Ext.extend(Ext.Panel, {
         if(Ext.isEmpty(this.day))
             this.day = now.getDate();
         
-        console.log(this.year + '-' + this.month + '-' + this.day);
-        
         if (!Ext.isArray(this.items)) {
             this.items = [];
         }
@@ -36,14 +34,45 @@ Ext.ux.Calendar = Ext.extend(Ext.Panel, {
         });
         gridTable += '</tr></thead>';
         
-        // date rows
+        var firstDayOfMonth = new Date(this.year, this.month, 1).getDay();
+        var lastDayOfMonth = this.daysInAMonth(this.year, this.month);
+        var currentDay = 1;    
+        var rows = [];
         for(var row = 0; row < 5; row++) {
-            gridTable += '<tr>';
+            rows[row] = [];
             for(var col = 0; col < 7; col++) {
-                gridTable += '<td>31</td>';
+                // don't start till the first day of the month
+                if(row == 0 && col < firstDayOfMonth) {
+                    rows[row][col] = null;
+                    continue;
+                }
+                // stop after the last day of the month
+                if(currentDay > lastDayOfMonth) {
+                    rows[row][col] = null;
+                    continue;
+                }
+                rows[row][col] = currentDay;
+                currentDay++;
+            }
+        }
+        
+        // date rows
+        Ext.each(rows, function(item){
+            gridTable += '<tr>';
+            for(col = 0; col < 7; col++) {
+                var num = '';
+                var isToday = '';
+                if(!Ext.isEmpty(item[col])) {
+                    num = item[col];
+                    
+                    // add a class for the selected day
+                    if(num == this.day)
+                        isToday = 'x-calendar-selected';
+                }
+                gridTable += '<td class="' + isToday + '">' + num + '</td>';
             }
             gridTable += '</tr>';
-        }
+        }, this);
         
         gridTable += '</table>';
         return {
@@ -53,6 +82,25 @@ Ext.ux.Calendar = Ext.extend(Ext.Panel, {
                 afterrender: this.initGridEvents
             }
         };
+    },
+    
+    daysInAMonth: function(year, month)  {
+        var days;
+        if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11)  days = 31;
+        else if (month == 3 || month == 5 || month == 8 || month == 10) days = 30;
+        else if (month == 1)  {
+            if (this.isLeapYear(year)) { days = 29; }
+            else { days = 28; }
+        }
+        return days;
+    },
+    
+    isLeapYear: function(year) {
+        if (((year % 4) == 0) && ((year % 100) != 0) || ((year % 400) == 0)) {
+            return true;
+        } else { 
+            return false; 
+        }
     },
     
     initGridEvents: function(cmp) {
